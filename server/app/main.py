c66d61ac411f65c686ace3c93aa47f041e4664b3
SUJOY GARAI <sujoycode999@gmail.com>
2025-08-19 10:13:27 +0530

    udate path

--- server/app/main.py
+++ server/app/main.py
@@ -6,7 +6,10 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+    "https://tcasystem.vercel.app",
+    "http://localhost:5173"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
