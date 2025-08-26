ae044389d47883a4166e944d00b3fe5fbd44d01b
SUJOY GARAI <sujoycode999@gmail.com>
2025-08-26 19:55:40 +0530

    Refactor CORS settings in main.py and update binary files
    - Changed CORS middleware to allow all origins in main.py.
    - Updated compiled Python files in __pycache__ directories for config, main, database, course, teachers, admin, allotments, teacher, view, hash_pass, and jwt_handler.

--- server/app/main.py
+++ server/app/main.py
@@ -6,10 +6,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-    "https://tcasystem.vercel.app",
-    "http://localhost:5173"
-    ],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
